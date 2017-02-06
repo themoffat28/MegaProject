1fe45835a2b72b81b02b47d71216c34d5b41d8b0
themoffat28 <bthemoffat@gmail.com>
2017-02-06 09:23:19 -0700

    Added header file for IntNode class

---
+++ MegaProject/Model/IntNode.cpp
@@ -0,0 +1,9 @@
+//
+//  IntNode.cpp
+//  MegaProject
+//
+//  Created by Moffat, Braeden on 2/6/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#include "IntNode.hpp"
---
+++ MegaProject/Model/IntNode.hpp
@@ -0,0 +1,33 @@
+//
+//  IntNode.hpp
+//  MegaProject
+//
+//  Created by Moffat, Braeden on 2/6/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#ifndef IntNode_hpp
+#define IntNode_hpp
+
+class IntNode
+{
+private:
+    int nodeData();
+    IntNode * NodePointer();
+public:
+    //Constructors
+    IntNode();
+    IntNode(int value);
+    /*
+     Created an IntNode with a specified value and reference to the next node.
+     */
+    IntNode(int value, IntNode * nextNode);
+    //Methods
+    int getNodeData();
+    IntNode * getNodePointer();
+    
+    void setNodeData(int value);
+    void setNodePointer(IntNode * updatedNode);
+};
+
+#endif /* IntNode_hpp */

2f580815acd02035915f359199104e0a967131d4
themoffat28 <bthemoffat@gmail.com>
2017-02-08 08:47:25 -0700

    Created Header file (and Umlet) for IntNodeArray.

---
+++ MegaProject/IntNodeArray.cpp
@@ -0,0 +1,9 @@
+//
+//  IntNodeArray.cpp
+//  MegaProject
+//
+//  Created by Moffat, Braeden on 2/8/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#include "IntNodeArray.hpp"
---
+++ MegaProject/IntNodeArray.hpp
@@ -0,0 +1,26 @@
+//
+//  IntNodeArray.hpp
+//  MegaProject
+//
+//  Created by Moffat, Braeden on 2/8/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#ifndef IntNodeArray_hpp
+#define IntNodeArray_hpp
+
+#include "IntNode.hpp"
+
+class IntNodeArray
+{
+private:
+    IntNode * head;
+    int size;
+public:
+    IntNodeArray(int bigness);
+    int getSize();
+    int getFromIndex(int index);
+    void setAtIndex(int index, int value);
+};
+
+#endif /* IntNodeArray_hpp */

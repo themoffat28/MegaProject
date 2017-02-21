a88e177d14102d2361de71a3f025b2f731e645c8
themoffat28 <bthemoffat@gmail.com>
2017-02-21 08:53:24 -0700

    Wrote testAdvancedFeatures method.

--- MegaProject/Controller/StructureController.cpp
+++ MegaProject/Controller/StructureController.cpp
@@ -70,3 +70,20 @@ void StructureController :: start()
     cout << "IntNodeArray test complete." << endl;
     testNodeTypes();
 }
+
+void StructureController :: testAdvancedFeatures()
+{
+    int showDestructor = 0;
+    if(showDestructor < 1)
+    {
+        Array<string> words = Array<string>(5);
+        cout << "There should be messages about destructor next" << endl;
+    }
+    Array<string> words = Array<string>(4);
+    words.setAtIndex(0, "at zero");
+    words.setAtIndex(3, "in the last spot");
+    Array<string> copiedWords = Array<string>(words);
+    
+    cout << "The following should match:" << endl;
+    cout << words.getFromIndex(0) << " should be the same as " << copiedWords.getFromIndex(0) << endl;
+}

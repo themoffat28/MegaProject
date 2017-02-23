cdee5973bb6ddacfc4f1ca1a7c490860ff35aa96
themoffat28 <bthemoffat@gmail.com>
2017-02-23 09:52:47 -0700

    Wrote remove method

--- MegaProject/Model/List.hpp
+++ MegaProject/Model/List.hpp
@@ -120,4 +120,52 @@ void List<Type> :: addAtIndex(int index, type value)
     
 }
 
+template <class Type>
+Type List<Type> :: remove(int index)
+{
+    assert(index >= 0 && index < size);
+    Type removed;
+    
+    Node<Type> * current = front;
+    Node<Type> * previous = nullptr;
+    Node<Type> * toBeRemoved = nullptr;
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        front = front->getNodePointer();
+    }
+    else if(index == size - 1)
+    {
+        for(int spot = 0; spot < index spot++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        toBeRemoved = current;
+        previous->setNodePointer(nullptr);
+        this->end = previous;
+    }
+    else
+    {
+        for(int spot = 0; spot < index spot++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        toBeRemoved = current;
+        current = toBeRemoved->getNodePointer();
+        previous->setNodePointer(current);
+    }
+    removed = toBeRemoved->getNodeData();
+    
+    delete toBeRemoved;
+    
+    size--;
+    return removed;
+}
+
+
+
 #endif /* List_h */

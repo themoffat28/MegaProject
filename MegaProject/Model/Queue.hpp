934c9f5a2e93831cd15e5c7c3ed497cc2bdf99cb
themoffat28 <bthemoffat@gmail.com>
2017-03-07 08:43:13 -0700

    Wrote dequeue & peek methods for Queue.hpp

--- MegaProject/Model/Queue.hpp
+++ MegaProject/Model/Queue.hpp
@@ -14,6 +14,52 @@
 template <class Type>
 class Queue : public DoublyLinkedList<Type>
 {
+private:
     
+public:
+    Queue();
+    void add(Type data);
 };
+
+//1.Assert size is valid.
+//2.Get data from front node.
+//If size == 1, set end to nullptr.
+//4.Else, move front to front's next.
+//4a.Set front's previous to nullptr.
+//5.Delete old front node.
+//6.Adjust size down by 1.
+//7.Return old value.
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    assert(this->getSize() > 0);
+    
+    Type removedValue = this->getFront()->getNodeData();
+    BiDirectionalNode<Type> * removeMe = this->getFront();
+    
+    if(this->getSize() == 1)
+    {
+        this->setEnd(nullptr);
+        this->setFront(nullptr);
+    }
+    else
+    {
+        this->setFront(removeMe->getNextPointer());
+    }
+    this->setFront()->setPreviousPointer(nullptr);
+    
+    delete removeMe;
+    this->setSize(this->getSize() -1);
+    
+    return removedValue;
+}
+
+//1.Check that size is greater than 0.
+//2.Return the front object's data.
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->getSize() > 0);
+    return this->getFront()->getNodeData():
+}
 #endif /* Queue_h */

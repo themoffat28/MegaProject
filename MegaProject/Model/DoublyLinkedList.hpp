da51b9a67c369541589d4f2f8bfcbeeba858cf07
themoffat28 <bthemoffat@gmail.com>
2017-03-07 07:22:10 -0700

    Changed DoublyLinkedList

--- MegaProject/Model/DoublyLinkedList.hpp
+++ MegaProject/Model/DoublyLinkedList.hpp
@@ -8,25 +8,31 @@
 
 #ifndef DoublyLinkedList_h
 #define DoublyLinkedList_h
+
 #include "BiDirectionalNode.hpp"
 
 template <class Type>
 class DoublyLinkedList
 {
 private:
-    int size;
     BiDirectionalNode<Type> * front;
     BiDirectionalNode<Type> * end;
+    int size;
 public:
     virtual void add(Type value) = 0;
     virtual Type remove(int index) = 0;
     DoublyLinkedList();
-    virtual ~DoublyLinkedList() = 0;
+    virtual ~DoublyLinkedList();
     
     int getSize() const;
     BiDirectionalNode<Type> * getFront() const;
     BiDirectionalNode<Type> * getEnd() const;
+    
+    void setFront(BiDirectionalNode<Type> * front);
+    void setSize(int updated);
+    void setEnd(BiDirectionalNode<Type> * end);
 };
+
 
 template <class Type>
 DoublyLinkedList<Type> :: DoublyLinkedList()
@@ -36,4 +42,77 @@ DoublyLinkedList<Type> :: DoublyLinkedList()
     this->end = nullptr;
 }
 
+template <class Type>
+DoublyLinkedList<Type> :: ~DoublyLinkedList()
+{
+    //Implemented only to avoid errors.
+    //Just like a Java interface method.
+}
+
+template <class Type>
+int DoublyLinkedList<Type> :: getSize() const
+{
+    return this->size;
+}
+
+template <class Type>
+BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getFront() const
+{
+    return this->front;
+}
+
+template <class Type>
+BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getEnd() const
+{
+    return this->end;
+}
+
+template <class Type>
+void DoublyLinkedList<Type> :: setSize(int size)
+{
+    this->size = size;
+}
+
+template <class Type>
+void DoublyLinkedList<Type> :: setFront(BiDirectionalNode<Type> * front)
+{
+    this->front = front;
+}
+
+template <class Type>
+void DoublyLinkedList<Type> :: setEnd(BiDirectionalNode<Type> * end)
+{
+    this->end = end;
+}
 #endif /* DoublyLinkedList_h */
+
+
+
+-=-=-=-=-=-=
+
+
+
+/*
+ Adds the supplied object to the stack to the end.
+ Set new object to point to end.
+ 
+ Adjusts the end pointer to reflect the new end of the stack.
+ Increases the size by 1.
+ */
+template <class Type>
+void Stack<Type> :: push(Type addedThing)
+{
+    BiDirectionalNode<Type> * addToStack = new BiDirectionalNode<Type>(addedThing);
+    
+    if(this->getSize() == 0 || this->getFront() == nullptr || this->getEnd() == nullptr)
+    {
+        this->setFront(addToStack);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(addToStack);
+        addToStack->setPreviousPointer(this->getEnd());
+    }
+    this->setEnd(addToStack);
+    this->setSize(this->getSize() + 1);
+}

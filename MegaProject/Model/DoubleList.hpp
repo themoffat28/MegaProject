7e5d8892540c9b4e08edb0a2cbe8af6d1b30970c
themoffat28 <bthemoffat@gmail.com>
2017-03-07 09:56:36 -0700

    Working on DoubleList().

--- MegaProject/Model/DoubleList.hpp
+++ MegaProject/Model/DoubleList.hpp
@@ -14,7 +14,60 @@
 template <class Type>
 class DoubleList : public DoublyLinkedList<Type>
 {
-    
+private:
+public:
+    DoubleList();
+    ~DoubleList();
+    void add(Type data);
+    Type remove(int index);
+    void addAtIndexFast(int index, Type value);
+    void addAtIndex(int index, Type value);
+    Type getFromIndex(int index);
+    Type getFromIndexFast(int index);
 };
 
+template <class Type>
+DoubleList<Type> :: DoubleList() : DoublyLinkedList<Type>()
+{
+    
+}
+
+template <class Type>
+DoubleList<Type> :: ~DoubleList()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer();
+        delete remove;
+        remove = this->getFront;
+    }
+}
+
+template <class Type>
+                       Type DoubleList<Type> :: getFromIndexFast(int index)
+                       {
+                           assert(index >= 0 && index < this->getSize());
+                           Type valueAtIndex;
+                           BiDirectionalNode<Type> * reference;
+                           if(index < this->getSize() / 2)
+                           {
+                               reference = this->getFront();
+                               for(int position = 0; position < index; position++)
+                               {
+                                   reference = reference->getNextPointer();
+                               }
+                           }
+                           else
+                           {
+                               reference = this->getEnd();
+                               for(int position = this->getSize() - 1; position > index; position--)
+                               {
+                                   reference = reference->getPreviousPointer();
+                               }
+                           }
+                       }
+        
+                       valueAtIndex = reference->getNodeData();
+                       return valueAtIndex;
 #endif /* DoubleList_h */
